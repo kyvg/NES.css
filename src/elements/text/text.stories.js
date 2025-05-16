d776076398f34bcb7703f50ff51036da94fca789
Evgeny Kirpichyov <47474371+kyvg@users.noreply.github.com>
2025-05-16 21:31:35 +0300

    add text element

---
+++ src/elements/text/text.stories.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'Elements/Text',
+};
+
+export const Showcase = {
+  render: () => `
+    <div class="stack">
+      <span class="nes-text is-primary">Primary</span>
+      <span class="nes-text is-success">Success</span>
+      <span class="nes-text is-warning">Warning</span>
+      <span class="nes-text is-danger">Error</span>
+    </div>
+  `,
+};
